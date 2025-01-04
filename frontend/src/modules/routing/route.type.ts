751f58ec2b3421821b09ceec9512a6ae45638ad5
idoRevah <ido.revah@gmail.com>
2025-01-04 18:07:07 +0200

    feat(dynamic router with params)

--- frontend/src/modules/routing/route.type.ts
+++ frontend/src/modules/routing/route.type.ts
@@ -3,5 +3,6 @@ import React from "react";
 export default interface Route {
   path: string;
   text: string;
-  element: JSX.Element;
+  element: any;
+  isVisible: boolean;
 }
---
+++ frontend/src/pages/post.tsx
@@ -0,0 +1,11 @@
+import { useParams } from "react-router-dom";
+const Post = () => {
+  const { id } = useParams();
+  return (
+    <div>
+      post
+      {id}
+    </div>
+  );
+};
+export default Post;
