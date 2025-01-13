58821565bf47c253adb8584006b3e505f994cbfa
ohad samu <ohadsamu@gmil.com>
2025-01-13 22:08:23 +0200

    feat: https support

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,25 @@
 import app from './app';
+import fs from 'fs';
+import http from 'http';
+import https from 'https';
+import dotenv from 'dotenv';
+
+// Load environment variables
+dotenv.config();
 
 const PORT = process.env.PORT || 3000;
+const ENV = process.env.NODE_ENV || 'development';
 
-app.listen(PORT, () => {
-  console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`);
-});
+if (ENV === 'production') {
+  const key = fs.readFileSync('./client-key.pem', 'utf8');
+  const cert = fs.readFileSync('./client-cert.pem', 'utf8');
+  const credentials = { key, cert };
+
+  https.createServer(credentials, app).listen(PORT, () => {
+    console.log(`Server is running in production mode with HTTPS on port ${PORT}`);
+  });
+} else {
+  http.createServer(app).listen(PORT, () => {
+    console.log(`Server is running in development mode with HTTP on port ${PORT}`);
+  });
+}
