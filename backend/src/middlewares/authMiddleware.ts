d6b556f71de0e6a7ce4550463f853af976a64ce5
ohad samu <ohadsamu@gmil.com>
2025-02-03 21:40:00 +0200

    deubg: comment out auth for dev

--- backend/src/middlewares/authMiddleware.ts
+++ backend/src/middlewares/authMiddleware.ts
@@ -1,20 +1,22 @@
 // src/middleware/authMiddleware.ts
-import jwt from 'jsonwebtoken';
-import User from '../models/userModel';
+// import jwt from 'jsonwebtoken';
+// import User from '../models/userModel';
 
+// TODO: return after developing
 export const authenticate = async (req: any, res: any, next: any) => {
-  const token = req.header('Authorization')?.replace('Bearer ', '');
-  if (!token) return res.status(401).json({ message: 'No token provided' });
+  next();
+  // const token = req.header('Authorization')?.replace('Bearer ', '');
+  // if (!token) return res.status(401).json({ message: 'No token provided' });
 
-  try {
-    console.log('Token:', token); // Debugging: Log the token
-    const decoded: any = jwt.verify(token, process.env.JWT_SECRET || '');
-    console.log('Decoded:', decoded); // Debugging: Log decoded data
-    req.user = await User.findById(decoded.id); 
-    if (!req.user) return res.status(401).json({ message: 'Invalid token' });
-    next();
-  } catch (err) {
-    console.error('Authentication Error:', err); // Debugging
-    res.status(401).json({ message: 'Unauthorized' });
-  }
+  // try {
+  //   console.log('Token:', token); // Debugging: Log the token
+  //   const decoded: any = jwt.verify(token, process.env.JWT_SECRET || '');
+  //   console.log('Decoded:', decoded); // Debugging: Log decoded data
+  //   req.user = await User.findById(decoded.id); 
+  //   if (!req.user) return res.status(401).json({ message: 'Invalid token' });
+  //   next();
+  // } catch (err) {
+  //   console.error('Authentication Error:', err); // Debugging
+  //   res.status(401).json({ message: 'Unauthorized' });
+  // }
 };
