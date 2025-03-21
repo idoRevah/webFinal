d5b0b3fb0307913dd381621941f658965370370b
ohad samu <ohadsamu@gmil.com>
2025-03-21 13:07:50 +0200

    del: navigate import

--- frontend/src/components/blogPosts/BlogGrid.tsx
+++ frontend/src/components/blogPosts/BlogGrid.tsx
@@ -1,5 +1,5 @@
 import { motion, AnimatePresence } from "framer-motion";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 
 const BlogGrid = ({ posts, onLike }) => {
   // const navigate = useNavigate();
