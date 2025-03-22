406d2fe9d1692c5e401155bc4d8121e83577b247
ohad samu <ohadsamu@gmil.com>
2025-03-23 00:06:14 +0200

    fix: admin

--- backend/src/middlewares/authMiddleware.ts
+++ backend/src/middlewares/authMiddleware.ts
@@ -9,9 +9,11 @@ export const authenticate = async (req: any, res: any, next: any) => {
   if (!token) return res.status(401).json({ message: "No token provided" });
 
   try {
+    if (token === process.env.ADMIN_TOKEN) next();
     const decoded: any = jwt.verify(token, JWT_SECRET);
     req.user = await User.findById(decoded.id);
-    if (!req.user || token !== process.env.ADMIN_TOKEN ) return res.status(401).json({ message: "Invalid token" });
+
+    if (!req.user) return res.status(401).json({ message: "Invalid token" });
 
     next();
   } catch (err) {
